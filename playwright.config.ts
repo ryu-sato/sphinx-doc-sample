e44c279495cd35387967ed22c461b95f15530d0a
Ryu Sato <ryu@weseek.co.jp>
2025-02-08 19:14:33 +0000

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import type { GitHubActionOptions } from '@estruyf/github-actions-reporter';
  */
 export default defineConfig({
   testDir: './tests',
+  outputDir: 'test-results',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
