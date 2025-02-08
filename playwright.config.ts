940dc627db6e8158618d925ed389505a3a89b501
Ryu Sato <ryu@weseek.co.jp>
2025-02-08 15:10:00 +0000

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run run-app:en',
+    command: 'yarn run-app:en',
     url: 'http://127.0.0.1:8000',
     reuseExistingServer: !process.env.CI,
   },
